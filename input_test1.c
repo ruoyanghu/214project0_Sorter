c4b39b6edb0cc74051714508abc49a2d9f24426f
janusgreenb97 <rh522@scarletmail.rutgers.edu>
2017-09-19 16:09:22 -0400

    First commit

---
+++ input_test1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+ int main(int argc, char *argv[]) 
+{
+    char input1[50];
+    char input2[50];
+	scanf("%s",&input1);
+	scanf("%s",&input2);
+	printf("input1: %s input2: %s\n",input1,input2);
+}
