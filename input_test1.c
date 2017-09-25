eca79f7b7ee7960ebbd02f2878ab568c114145df
janusgreenb97 <rh522@scarletmail.rutgers.edu>
2017-09-24 23:33:46 -0400

    scanWillEditSoon

--- input_test1.c
+++ input_test1.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+void cleanBuffer(char* buffer, int size);
+void cleanBuffer(char* buffer, int size){
+	int i = 0;
+	while(i!=size-1){
+		buffer[i] = '\0';
+		i++;
+	}
+}
  int main(int argc, char *argv[]) 
 {
-    char input1[50];
-    char input2[50];
-	scanf("%s",&input1);
-	scanf("%s",&input2);
-	printf("input1: %s input2: %s\n",input1,input2);
+    
+    char* test = (char *)malloc(sizeof(char) * 100);
+	cleanBuffer(test,100);
+	test[0]= '0';
+	test[1]= '1';
+	test = '.'+test;
+
+	
+	printf("the test is: %s \n",test);
 	
 }
